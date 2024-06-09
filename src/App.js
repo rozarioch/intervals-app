b255ffa3e1dd1f877e54923d2d4a24484efffd3f
Dmitry <chaykovskiy.d.l@gmail.com>
2024-06-09 09:55:12 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Activity from "./Activity";
+import styles from "./App.module.css";
+import ActivityForm from "./components/ActivityForm/ActivityForm";
+import ActivityList from "./components/ActivityList/ActivityList";
+import TextToSpeech from "./components/TextToSpeech";
 
 function App() {
+  const [activityList, setActivityList] = useState([]);
+
+  const onAddItem = (props) => {
+    setActivityList((prev) => [
+      ...prev,
+      { name: props.name, seconds: props.seconds },
+    ]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.root}>
+      <h1>Activity Timer App</h1>
+      <ActivityForm onAddItem={onAddItem} />
+      <ActivityList list={activityList} />
+      {/* <TextToSpeech text={"asd"} /> */}
     </div>
   );
 }
